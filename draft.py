09d468bbf7e6aaab76da28e2ec8aa77bf40fa073
Felipe Rodrigues <felipe@felipevr.com>
2020-03-13 16:23:40 -0300

    Atualiza dados e corrige formatação de data

--- draft.py
+++ draft.py
@@ -1,8 +1,13 @@
 import pandas as pd
-import seaborn as sns
+import seaborn as sns; sns.set()
 
-sns.set()
-
+# Lê e formata os dados
 df = pd.read_csv("data/ministerio_saude.csv", sep=";")
-df.data = pd.to_datetime(df.data)
-sns.lineplot(data=df, x="data", y="qtde_confirmados", markers=True, dashes=False)
+df.data = pd.to_datetime(df.data, format="%d/%m/%Y")
+
+# Monta o gráfico
+plot = sns.lineplot(data=df, x="data", y="qtde_confirmados", markers=True, dashes=False)
+
+# Salva o gráfico
+figure = plot.get_figure()
+figure.savefig("casos_confirmados.png")
